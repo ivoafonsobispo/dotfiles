ec200bfeeea6a21b012a7707f98bdf3f7ddc114a
Ivo Afonso Bispo <ivoafobispo@gmail.com>
2023-10-26 11:49:35 +0100

    Update configure_workspace_shortcuts.sh

--- configure_workspace_shortcuts.sh
+++ configure_workspace_shortcuts.sh
@@ -3,11 +3,11 @@
 # Disable dynamic workspaces
 gsettings set org.gnome.mutter dynamic-workspaces false
 
-# Set number of workspaces to 10
-gsettings set org.gnome.desktop.wm.preferences num-workspaces 10
+# Set number of workspaces to 9
+gsettings set org.gnome.desktop.wm.preferences num-workspaces 9
 
 # Clear switch-to-application keybindings
-for i in {1..10}; do
+for i in {1..9}; do
     gsettings set org.gnome.shell.keybindings "switch-to-application-$i" []
 done
 
